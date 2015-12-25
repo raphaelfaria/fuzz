6040a81244a99c3bd7932c86e923531dcff2eeaa
Raphael Faria <raphaelfmf@gmail.com>
2015-12-25 11:14:42 -0200

    Starts conversion to ES6

--- fuzz.js
+++ fuzz.js
@@ -1,145 +1,136 @@
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else if (typeof exports === 'object') {
-        module.exports = factory();
-    } else {
-        var oldFuzz = window.Fuzz;
-        var api = window.Fuzz = factory();
-        api.noConflict = function () {
-            window.Fuzz = oldFuzz;
-            return api;
-        };
+class Item {
+  constructor(name, index) {
+    this.name = name;
+    this.mainIndex = index;
+    this.weight = 0;
+    this.nameArray = this.name.split('');
+    this.detailedArray = this._prepareItem();
+  }
+
+  _prepareItem() {
+    function indexIsSectionStart(string, index) {
+      return index === 0 ||
+        (string[index].toUpperCase() === string[index] &&
+          (index === 1 ||
+            string[index - 1].toUpperCase() !== string[index - 1]));
     }
-}(function () {
-    // Internal functions
 
-    var prepareCollection = function (collection) {
-        var _this = this;
-        var newCollection = [];
-        var l = collection.length;
+    return this.nameArray.map((char, index) => {
+      return {
+        index, char,
+        beginSection: indexIsSectionStart(this.nameArray, index),
+        weight: 0
+      }
+    });
+  }
 
-        for (var i = 0; i < l; i++) {
-            newCollection.push(new Item(collection[i], i));
+  calcMatch(string) {
+    var matchIndex = [];
+    var searchIndex = -1;
+    var lookUpper = true;
+    var testName = this.name;
+    var lowerTestName = this.name.toLowerCase();
+
+    for (var i = 0; i < string.length; i++) {
+        var currentChar = string.charAt(i);
+
+        if (lookUpper) {
+            testName = this.name;
+            currentChar = currentChar.toUpperCase();
         }
 
-        return newCollection;
-    };
+        if (!lookUpper) testName = lowerTestName;
 
-    var indexIsSectionStart = function (string, index) {
-        return  index === 0 ||
-                (string[index].toUpperCase() === string[index] &&
-                    (index === 1 ||
-                     string[index - 1].toUpperCase() !== string[index - 1]));
-    };
+        if (searchIndex >= this.name.length) return false;
 
-    var prepareItem = function (nameArray) {
-        var detailedItem = [];
-        var detailedChar;
-        var l = nameArray.length;
-        for (var i = 0; i < l; i++) {
-            detailedChar = {
-                index: i,
-                c: nameArray[i],
-                beginSection: indexIsSectionStart(nameArray, i),
-                w: 0
-            };
+        searchIndex = testName.indexOf(currentChar, searchIndex + 1);
 
-            detailedItem.push(detailedChar);
-        }
-
-        return detailedItem;
-    };
-
-
-    // Main obj
-    var Fuzz = function (collection, options) {
-        options || (options = {});
-
-        this.main = collection;
-        this.collection = prepareCollection.call(this, this.main);
-
-        return this;
-    };
-
-    Fuzz.prototype.match = function (string) {
-        var input = string;
-        var query = input.replace(/\s+/g, '').toLowerCase();
-
-        var l = this.collection.length;
-        var results = [];
-
-        for (var i = 0; i < l; i++) {
-            var match = this.collection[i].match(query);
-
-            if (match) results.push(this.collection[i]);
-        }
-
-        return results;
-    };
-
-
-    // Item obj
-    var Item = function(name, index) {
-        this.name = this.parse(name);
-        this.nameArray = this.name.split('');
-        this.detailed = prepareItem(this.nameArray);
-
-        return this;
-    };
-
-    Item.prototype.parse = function(item) {
-        return item;
-    };
-
-    Item.prototype.clearWeight = function () {
-        var l = this.detailed.length;
-        for (var i = 0; i < l; i++) {
-            this.detailed[i].w = 0;
-        }
-    };
-
-    Item.prototype.match = function (string) {
-        var matchIndex = [];
-        var searchIndex = -1;
-        var lookUpper = true;
-        var testName = this.name;
-
-        for (var i = 0; i < string.length; i++) {
-            var currentChar = string.charAt(i);
-
-            if (lookUpper) {
-                testName = this.name;
-                currentChar = currentChar.toUpperCase();
+        if (searchIndex > -1) {
+            matchIndex.push(searchIndex);
+        } else {
+            if (lookUpper === false) {
+                matchIndex = [];
+                break;
             }
 
-            if (!lookUpper) testName = testName.toLowerCase();
-
-            if (searchIndex >= this.name.length) return false;
-
-            searchIndex = testName.indexOf(currentChar, searchIndex + 1);
-
-            if (searchIndex > -1) {
-                matchIndex.push(searchIndex);
-            } else {
-                if (lookUpper === false) {
-                    matchIndex = [];
-                    break;
-                }
-
-                if (lookUpper === true) {
-                    i -= 1;
-                    searchIndex = matchIndex[matchIndex.length -1] || -1;
-                }
-
-                lookUpper = !lookUpper;
+            if (lookUpper === true) {
+                i -= 1;
+                searchIndex = matchIndex[matchIndex.length -1] || -1;
             }
+
+            lookUpper = !lookUpper;
+        }
+    }
+
+    this.matched = matchIndex.length ? matchIndex : false;
+
+    // Calculate rank
+    this.calculateWeight();
+
+    return !!(this.matched.length);
+  }
+
+  calculateWeight() {
+    let l = this.matched.length;
+    let index;
+    let substringSize = 0;
+
+    for (let i = 0; i < l; i++) {
+        index = this.matched[i];
+
+        if (this.set[index].beginSection === true) {
+            this.detailedArray[index].weight = (80 - index);
+            this.weight += this.detailedArray[index].weight;
+
+            if (this.matched[index - 1] == index - 1) {
+                substringSize++;
+                this.detailedArray[index].weight += 15 * Math.pow(2, substringSize) - index;
+                this.weight += this.detailedArray[index].weight;
+            }
+        } else if (this.matched[index - 1] == index - 1) {
+            substringSize++;
+            this.detailedArray[index].weight = 15 * Math.pow(2, substringSize) - index;
+            this.weight += this.detailedArray[index].weight;
+        } else {
+            this.detailedArray[index].weight = 10 - index;
+            this.weight += this.detailedArray[index].weight;
         }
 
-        this.matched = matchIndex.length ? matchIndex : false;
+        this.weight -= ((this.length - 1) - this.matched[this.matched.length - 1]);
+    }
 
-        return !!(this.matched.length);
-    };
+    return this;
+  }
+}
 
-    return Fuzz;
-}));
+class Fuzz extends Array {
+  constructor(collection, options = {}) {
+    super();
+    this.main = collection;
+    this.push.apply(this, this._prepareCollection());
+  }
+
+  parse(item) {
+    return item;
+  }
+
+  _prepareCollection() {
+    return this.main.map((item, i) => {
+      return new Item(this.parse(item), i);
+    });
+  }
+
+  match(string) {
+    let query = string.replace(/\s+/g, '').toLowerCase();
+    let results = this.filter(item => item.calcMatch(query));
+    return results.sort((a, b) => {
+      if (a.weight > b.weight)
+        return -1;
+      if (a.weight < b.weight)
+        return 1;
+      return 0;
+    });
+  }
+}
+
+export default Fuzz;
